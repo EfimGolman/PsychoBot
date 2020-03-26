0314b4f23c852e7fdcad8e797d4948c190426fa3
EfimGolman <golmanefim@gmail.com>
2020-03-26 18:34:53 +0300

    pre-deploy

--- bot.js
+++ bot.js
@@ -1,33 +1,36 @@
 const TelegramBot = require('node-telegram-bot-api');
 const mongoose = require('mongoose');
-require('dotenv').config()
+require('dotenv').config();
 
-const token = '956069730:AAGqHzw-6C3rFgPZ1NsvcuwRQROXo0GEVzo';
+console.log('Bot started!');
+
+const token = process.env.DB_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 const Question = require('./model/question');
 
 const notes = [];
-
+let questionNumber;
+let questionItem;
+let arrayAnswers = [];
 
 bot.getMe().then((me) => {
   console.log('Hello! My name is %s!', me.first_name);
   console.log('My id is %s.', me.id);
   console.log('And my username is @%s.', me.username);
-  // bot.sendMessage(userId, 'Дядя Ёся рад Вас видеть. Начнем!?)');
 });
 
 const options = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [{ text: 'Я на нервяке', callback_data: 'Я на нервяке' }],
-      [{ text: 'Пройти мини тест', callback_data: 'Пройти минитест' }],
-      [{ text: 'Пройти большой тест', callback_data: 'Пройти большой тест' }],
+  parse_mode: 'Markdown',
+  reply_markup: {
+    keyboard: [
+      [{ text: 'Я на нервяке' }],
+      [{ text: 'Пройти мини-тест' }],
+      [{ text: 'Пройти большой тест' }],
     ],
-  }),
+  },
 };
 
 bot.onText(/\/start/, async (msg, match) => {
-
   const userId = msg.from.id;
   const text = match[1];
   const time = match[2];
@@ -36,99 +39,194 @@ bot.onText(/\/start/, async (msg, match) => {
 
   await bot.sendMessage(userId, 'Дядя Ёся рад тебя видеть. Начнем!? :)');
   await bot.sendMessage(userId, 'Я здесь не настолько разговорчив, как в жизни. \n\nЕсть всего 3 варианта развития событий:');
-  await bot.sendMessage(userId, '1. Нажми "Пройти мини тест" - и я быстро тебя протестирую.');
+  await bot.sendMessage(userId, '1. Нажми "Пройти мини-тест" - и я быстро тебя протестирую.');
   await bot.sendMessage(userId, '2. Нажмешь "Пройти большой тест" и мы залипнем надолго.');
   await bot.sendMessage(userId, '3. Отправь мне "Я на нервяке" и я постараюсь тебя успокоить.');
   await bot.sendMessage(userId, 'Выбирай!', options);
 });
 
-
-bot.on('callback_query', async (msg) => {
-  // console.log(msg.data);
-  const ans = msg.data;
-  const sum = 0;
-  let arrayAnswers = [];
-  if (ans === 'Я на нервяке') {
+bot.onText(/.+/g, async (msg, match) => {
+  if (match[0] === 'Я на нервяке') {
     await bot.sendMessage(msg.from.id, 'Вообще-то, всем сейчас тяжело!');
+    await bot.sendMessage(msg.from.id, 'Выбери что-то еще!');
   }
 
-  if (ans === 'Пройти большой тест') {
+  if (match[0] === 'Пройти большой тест') {
     await bot.sendMessage(msg.from.id, 'Тест состоит из 200 вопросов, займет у Вас около часа, отвлекаться нельзя! Готовы?');
-    setTimeout(async () => { await bot.sendMessage(msg.from.id, 'А я нет!!! Мне лень... :)))'); }, 5000);
-    setTimeout(async () => { await bot.sendMessage(msg.from.id, 'Выбери короткий тест', options); }, 5000);
+    setTimeout(async () => { await bot.sendMessage(msg.from.id, 'А я нет!!! Мне лень... :)))'); }, 3000);
+    setTimeout(async () => { await bot.sendMessage(msg.from.id, 'Выбери короткий тест', options); }, 3000);
   }
-  if (ans === 'Пройти минитест') {
+
+  if (match[0] === 'Пройти мини-тест') {
     const optionsMini0 = {
-      reply_markup: JSON.stringify({
-        inline_keyboard: [
-          [{ text: 'Да. Я хочу пройти минитест', callback_data: 'Да. Я хочу пройти минитест' }],
+      parse_mode: 'Markdown',
+      reply_markup: {
+        keyboard: [
+          [{ text: 'Да. Я хочу пройти мини-тест', callback_data: 'Да. Я хочу пройти мини-тест' }],
+
         ],
-      }),
+      },
     };
-    await bot.sendMessage(msg.from.id, 'Тест состоит из 12 вопросов, займет у Вас 3 минуты, но нельзя отвлекаться! Готовы? (Жмите "ДА")', optionsMini0);
+    await bot.sendMessage(msg.from.id, 'Тест состоит из 14 вопросов, займет у Вас 3 минуты, но нельзя отвлекаться! Готовы? (Жмите "ДА")', optionsMini0);
   }
-  if (ans === 'Да. Я хочу пройти минитест') {
-    
-    let questionItem;
-    mongoose.connect('mongodb://localhost:27017/miniTest', { useNewUrlParser: true, useUnifiedTopology: true });
 
+  mongoose.connect(`mongodb+srv://${process.env.DB_DATA}@cluster0-zioju.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
+  const questions = await Question.find({ });
 
-    let questionNumber = 1;
-    questionItem = await Question.findOne({ questionNumber }, {
-      question: 1, answer1: 1, answer2: 1, answer3: 1, answer4: 1,
-    });
-
-    let options = {
-      reply_markup: JSON.stringify({
-        inline_keyboard: [
-          [{ text: questionItem.answer1[1], callback_data: questionItem.answer1[2] }],
-          [{ text: questionItem.answer2[1], callback_data: questionItem.answer2[2] }],
-          [{ text: questionItem.answer3[1], callback_data: questionItem.answer3[2] }],
-          [{ text: questionItem.answer4[1], callback_data: questionItem.answer4[2] }],
+  if (match[0] === 'Да. Я хочу пройти мини-тест') {
+    questionNumber = 1;
+    questionItem = questions[questionNumber - 1];
+    const options1 = {
+      parse_mode: 'Markdown',
+      reply_markup: {
+        keyboard: [
+          [{ text: questionItem.answer1[1] }],
+          [{ text: questionItem.answer2[1] }],
+          [{ text: questionItem.answer3[1] }],
+          [{ text: questionItem.answer4[1] }],
         ],
-      }),
+      },
     };
 
-    setTimeout(async () => { await bot.sendMessage(msg.from.id, questionItem.question, options); }, 3000);
+    setTimeout(async () => { await bot.sendMessage(msg.from.id, questionItem.question, options1); }, 1000);
+  }
+  questionItem = questions[questionNumber - 1];
+  if (questionItem !== undefined && questionNumber < 15) {
 
-    bot.on('callback_query', async (msg) => {
-      arrayAnswers.push(msg.data);
+    switch (match[0]) {
 
-      console.log(arrayAnswers);
+      // CASE -  1
 
-      questionNumber++;
-      let res1 = 0; let res2 = 0;
-
-      if (questionNumber === 15) {
-        for (let i = 0; i < 7; i++) {
-          res1 += +arrayAnswers[i];
+      case (questionItem.answer1[1]): {
+        arrayAnswers.push(questionItem.answer1[2]);
+        questionNumber += 1;
+        if (questionNumber < 15) {
+          questionItem = questions[questionNumber - 1];
+          const options1 = {
+            parse_mode: 'Markdown',
+            reply_markup: {
+              keyboard: [
+                [{ text: questionItem.answer1[1] }],
+                [{ text: questionItem.answer2[1] }],
+                [{ text: questionItem.answer3[1] }],
+                [{ text: questionItem.answer4[1] }],
+              ],
+            },
+          };
+          setTimeout(async () => { await bot.sendMessage(msg.from.id, questionItem.question, options1); }, 100);
+          console.log(arrayAnswers);
         }
-        for (let i = 7; i < 14; i++) {
-          res2 += +arrayAnswers[i];
-        }
+        break;
+      }
 
-        console.log('Тревожность:', res1, 'Депрессия:', res2)
-        arrayAnswers = [];
-        await bot.sendMessage(msg.from.id,  `\u2605 \u2605 \u2605  Результаты: Тревожность: ${res1}, Депрессия:${res2} \u2605  \u2605  \u2605 `);
-        await bot.sendMessage(msg.from.id, `Если у тебя 7 и более баллов по каждому пункту => дядя Ёся ждет звонка +7 (985) 999-2364`);
-        await bot.sendMessage(msg.from.id, 'Чтобы начать сначала отправьте сообщение /start'); ;
+      // CASE -  2
+
+      case (questionItem.answer2[1]): {
+        arrayAnswers.push(questionItem.answer2[2]);
+        questionNumber += 1;
+        if (questionNumber < 15) {
+          questionItem = questions[questionNumber - 1];
+          const options1 = {
+            parse_mode: 'Markdown',
+            reply_markup: {
+              keyboard: [
+                [{ text: questionItem.answer1[1] }],
+                [{ text: questionItem.answer2[1] }],
+                [{ text: questionItem.answer3[1] }],
+                [{ text: questionItem.answer4[1] }],
+              ],
+            },
+          };
+          setTimeout(async () => { await bot.sendMessage(msg.from.id, questionItem.question, options1); }, 100);
+          console.log(arrayAnswers);
+        }
+        break;
       }
-      if (questionNumber < 15) {
-        questionItem = await Question.findOne({ questionNumber }, {
-          question: 1, answer1: 1, answer2: 1, answer3: 1, answer4: 1,
-        });
-        options = {
-          reply_markup: JSON.stringify({
-            inline_keyboard: [
-              [{ text: questionItem.answer1[1], callback_data: questionItem.answer1[2] }],
-              [{ text: questionItem.answer2[1], callback_data: questionItem.answer2[2] }],
-              [{ text: questionItem.answer3[1], callback_data: questionItem.answer3[2] }],
-              [{ text: questionItem.answer4[1], callback_data: questionItem.answer4[2] }],
-            ],
-          }),
-        };
-        await bot.sendMessage(msg.from.id, questionItem.question, options);
+
+      // CASE -  3
+
+      case (questionItem.answer3[1]): {
+        arrayAnswers.push(questionItem.answer3[2]);
+        questionNumber += 1;
+        if (questionNumber < 15) {
+          questionItem = questions[questionNumber - 1];
+          const options1 = {
+            parse_mode: 'Markdown',
+            reply_markup: {
+              keyboard: [
+                [{ text: questionItem.answer1[1] }],
+                [{ text: questionItem.answer2[1] }],
+                [{ text: questionItem.answer3[1] }],
+                [{ text: questionItem.answer4[1] }],
+              ],
+            },
+          };
+          setTimeout(async () => { await bot.sendMessage(msg.from.id, questionItem.question, options1); }, 100);
+          console.log(arrayAnswers);
+        }
+        break;
       }
-    });
+
+      // CASE -  4
+
+      case (questionItem.answer4[1]): {
+        arrayAnswers.push(questionItem.answer4[2]);
+        questionNumber += 1;
+        if (questionNumber < 15) {
+          questionItem = questions[questionNumber - 1];
+          const options1 = {
+            parse_mode: 'Markdown',
+            reply_markup: {
+              keyboard: [
+                [{ text: questionItem.answer1[1], callback_data: questionItem.answer1[2] }],
+                [{ text: questionItem.answer2[1], callback_data: questionItem.answer2[2] }],
+                [{ text: questionItem.answer3[1], callback_data: questionItem.answer3[2] }],
+                [{ text: questionItem.answer4[1], callback_data: questionItem.answer4[2] }],
+              ],
+            },
+          };
+          setTimeout(async () => { await bot.sendMessage(msg.from.id, questionItem.question, options1); }, 100);
+          console.log(arrayAnswers);
+        }
+        break;
+      }
+
+      default: {
+        console.log('Some shit happend!');
+        console.log('MATCH', match[0]);
+        console.log('Answer1 - ', questionItem.answer1[1]);
+        console.log('Answer2 - ', questionItem.answer2[1]);
+        console.log('Answer3 - ', questionItem.answer3[1]);
+        console.log('Answer4 - ', questionItem.answer4[1]);
+      
+      }
+    }
+  }
+  if (questionNumber === 15) {
+    let res1 = 0; let res2 = 0;
+    for (let i = 0; i < 7; i += 1) {
+      res1 += +arrayAnswers[i];
+    }
+    for (let i = 7; i < 14; i += 1) {
+      res2 += +arrayAnswers[i];
+    }
+
+    console.log('Тревожность:', res1, 'Депрессия:', res2);
+    const fin = {
+      parse_mode: 'Markdown',
+      reply_markup: {
+        keyboard: [
+          [{ text: 'Я на нервяке' }],
+          [{ text: 'Пройти мини-тест' }],
+          [{ text: 'Пройти большой тест' }],
+        ],
+      },
+    };
+
+    await bot.sendMessage(msg.from.id, `\u2605 \u2605 \u2605  Результаты: Тревожность: ${res1}, Депрессия:${res2} \u2605  \u2605  \u2605 `);
+    await bot.sendMessage(msg.from.id, 'Если у тебя 7 и более баллов по каждому пункту => дядя Ёся ждет звонка +7 (985) 999-2364');
+    await bot.sendMessage(msg.from.id, 'Чтобы начать сначала, жмите "Пройти мини-тест"', fin);
+    arrayAnswers = [];
+    questionNumber += 1;
   }
 });
--- seeders/seed.js
+++ seeders/seed.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const Question = require('../model/question');
 //const Test = require('../model/test');
 
-mongoose.connect('mongodb://localhost:27017/miniTest');
+mongoose.connect('mongodb+srv://Golmanefim:19121988eE@cluster0-zioju.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log).catch(console.error)
 async function seed() {
   const question1 = new Question({
-    question: 'Я испытываю напряжение, мне не по себе',
+    question: '1. Я испытываю напряжение, мне не по себе',
     questionNumber: 1,
     answer1: [1, 'Все время', 3],
     answer2: [2, 'Часто', 2],
@@ -15,7 +15,7 @@ async function seed() {
   });
 
   const question2 = new Question({
-    question: 'Я испытываю страх, кажется, что что-то ужасное может вот-вот случиться',
+    question: '2.Я испытываю страх, кажется, что что-то ужасное может вот-вот случиться',
     questionNumber: 2,
     answer1: [1, 'Определенно это так, и страх очень велик', 3],
     answer2: [2, 'Да, это так, но страх не очень велик', 2],
@@ -24,7 +24,7 @@ async function seed() {
   });
 
   const question3 = new Question({
-    question: 'Беспокойные мысли крутятся у меня в голове',
+    question: '3. Беспокойные мысли крутятся у меня в голове',
     questionNumber: 3,
     answer1: [1, 'Постоянно', 3],
     answer2: [2, 'Большую часть времени', 2],
@@ -34,7 +34,7 @@ async function seed() {
 
   const question4 = new Question({
     questionNumber: 4,
-    question: 'Я легко могу присесть и расслабиться',
+    question: '4. Я легко могу присесть и расслабиться',
     answer1: [1, 'Определенно, это так', 0],
     answer2: [2, 'Наверно, это так', 1],
     answer3: [3, 'Лишь изредка, это так', 2],
@@ -42,7 +42,7 @@ async function seed() {
   });
 
   const question5 = new Question({
-    question: 'Я испытываю внутренне напряжение или дрожь',
+    question: '5. Я испытываю внутренне напряжение или дрожь',
     questionNumber: 5,
     answer1: [1, 'Совсем не испытываю', 0],
     answer2: [2, 'Иногда', 1],
@@ -52,7 +52,7 @@ async function seed() {
 
 
   const question6 = new Question({
-    question: 'Я испытываю неусидчивость, мне постоянно нужно двигаться',
+    question: '6. Я испытываю неусидчивость, мне постоянно нужно двигаться',
     questionNumber: 6,
     answer1: [1, 'Определенно, это так', 3],
     answer2: [2, 'Наверно, это так', 2],
@@ -62,43 +62,43 @@ async function seed() {
 
   const question7 = new Question({
 
-    question: 'У меня бывает внезапное чувство паники',
+    question: '7. У меня бывает внезапное чувство паники',
     questionNumber: 7,
     answer1: [1, 'Очень часто', 3],
     answer2: [2, 'Довольно часто', 2],
-    answer3: [3, 'Не так уж часто ', 1],
+    answer3: [3, 'Не так уж часто', 1],
     answer4: [4, 'Совсем не бывает', 0],
   });
 
   const question8 = new Question({
-    question: 'То, что приносило мне большое удовольствие, и сейчас вызывает у меня такое же чувство',
+    question: '8. То, что приносило мне большое удовольствие, и сейчас вызывает у меня такое же чувство',
     questionNumber: 8,
     answer1: [1, 'Определенно, это так', 0],
     answer2: [2, 'Наверно, это так', 1],
-    answer3: [3, 'Лишь в некоторой степени это так ', 2],
+    answer3: [3, 'Лишь в некоторой степени это так', 2],
     answer4: [4, 'Это совсем не так', 3],
   });
 
   const question9 = new Question({
-    question: 'Я способен рассмеяться и увидеть в том или ином событии смешное',
+    question: '9. Я способен рассмеяться и увидеть в том или ином событии смешное',
     questionNumber: 9,
     answer1: [1, 'Определенно, это так', 0],
     answer2: [2, 'Наверно, это так', 1],
-    answer3: [3, 'Лишь в некоторой степени это так ', 2],
+    answer3: [3, 'Лишь в некоторой степени это так', 2],
     answer4: [4, 'Совсем не способен', 3],
   });
 
   const question10 = new Question({
-    question: 'Я испытываю бодрость',
+    question: '10. Я испытываю бодрость',
     questionNumber: 10,
     answer1: [1, 'Совсем не испытываю', 3],
     answer2: [2, 'Очень редко', 2],
-    answer3: [3, 'Иногда ', 1],
+    answer3: [3, 'Иногда', 1],
     answer4: [4, 'Практически все время', 0],
   });
 
   const question11 = new Question({
-    question: 'Мне кажется, что я стал все делать очень медленно',
+    question: '11. Мне кажется, что я стал все делать очень медленно',
     questionNumber: 11,
     answer1: [1, 'Практически все время', 3],
     answer2: [2, 'Иногда', 2],
@@ -107,7 +107,7 @@ async function seed() {
   });
 
   const question12 = new Question({
-    question: 'Я больше не слежу за своей внешностью                       ',
+    question: '12. Я больше не слежу за своей внешностью                       ',
     questionNumber: 12,
     answer1: [1, 'Определенно, это так', 3],
     answer2: [2, 'Я не уделяю этому столько времени, сколько нужно', 2],
@@ -116,7 +116,7 @@ async function seed() {
   });
 
   const question13 = new Question({
-    question: 'Я считаю, что мои дела (занятия, увлечения) могут принести мне чувство удовлетворения ',
+    question: '13. Я считаю, что мои дела (занятия, увлечения) могут принести мне чувство удовлетворения ',
     questionNumber: 13,
     answer1: [1, 'Точно так же, как и обычно', 0],
     answer2: [2, 'Да, но не втой степени, как раньше', 1],
@@ -125,7 +125,7 @@ async function seed() {
   });
 
   const question14 = new Question({
-    question: 'Я могу получить удовольствие от хорошей книги, радио- или телепрограммы ',
+    question: '14. Я могу получить удовольствие от хорошей книги, радио- или телепрограммы ',
     questionNumber: 14,
     answer1: [1, 'Часто', 0],
     answer2: [2, 'Иногда', 1],
@@ -133,10 +133,6 @@ async function seed() {
     answer4: [4, 'Очень редко', 3],
   });
 
-  // const miniTest = new Test({
-  //   testName: 'miniTest',
-  //   qustions: [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10, question11, question12, question13, question14],
-  // });
 
   await question1.save();
   await question2.save();
@@ -152,7 +148,7 @@ async function seed() {
   await question12.save();
   await question13.save();
   await question14.save();
-  // await miniTest.save();
+
 
   await mongoose.disconnect();
 }
